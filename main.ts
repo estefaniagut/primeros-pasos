4117ef0aa48eb38f1a7b4067955b843eaaa455e7
estefaniagut <110991132+estefaniagut@users.noreply.github.com>
2022-08-31 07:27:15 -0700

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,20 @@
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+basic.showIcon(IconNames.Asleep)
+basic.clearScreen()
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showIcon(IconNames.Heart)
+    basic.clearScreen()
+})
